b65842c49b126eeb0cded6108b3e76da8415a739
Nouman64-cat <noumanejazghost@gmail.com>
2024-10-21 02:17:24 +0500

    image optimization implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import HomePage from './components/HomePage'
-
+import Image from 'next/image'
+import image from '@/public/anime-girl-in-nature-5k-u9-2560x1440.jpg'
 const page = () => {
   return (
     <div>
+      <Image src={image} alt="" width={2560} height={1440} />
       <HomePage />
     </div>
   )
