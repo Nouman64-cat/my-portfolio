7d41226998850f2b51995252457834a08c949d50
Nouman64-cat <noumanejazghost@gmail.com>
2024-11-10 05:44:08 +0500

    stars height increase

--- app/components/general/StarBackground.tsx
+++ app/components/general/StarBackground.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const StarBackground: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const stars = Array.from({ length: 100 });
+  const stars = Array.from({ length: 200 });
 
   return (
     <div className="relative w-full h-full overflow-hidden">
@@ -13,7 +13,7 @@ const StarBackground: React.FC<{ children: React.ReactNode }> = ({ children }) =
             className="star"
             style={{
               left: `${Math.random() * 100}vw`,
-              top: `${Math.random() * 100}vh`,
+              top: `${Math.random() * 300}vh`,
               animationDuration: `${10 + Math.random() * 40}s`, // Varying duration for each star
               width: `${1 + Math.random() * 2}px`,
               height: `${1 + Math.random() * 2}px`,
